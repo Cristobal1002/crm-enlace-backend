59da15125ea115961ec3db48f99a818485af9bce
Cristobal1002 <cristobal1002@gmail.com>
2024-09-20 19:34:17 -0500

    Cambios posteriores a revision

--- src/validators/customer.validator.js
+++ src/validators/customer.validator.js
@@ -7,15 +7,14 @@ export const createCustomerValidator = [
     body(`document`).isNumeric().notEmpty().withMessage('Document is a required field and must be number'),
     body(`document_type`).isString().notEmpty().withMessage('Document type is a required field and must be number'),
     body(`phone`).isString().notEmpty().withMessage('Phone is a required field and must be string'),
-    body(`email`).isEmail().notEmpty().withMessage('Email is a required field and must be email'),
-    body(`birthday`).optional().notEmpty().withMessage('Birthday is a required field and must be string'),
-    body(`gender`).optional().isString().notEmpty().withMessage('Gender is a required field and must be string'),
+    body(`email`).optional().isEmail().withMessage('Email is a required field and must be email'),
+    body(`birthday`).optional(),
+    body(`gender`).optional().isString().withMessage('Gender is a required field and must be string'),
     body(`country_id`).isInt().notEmpty().withMessage('Country is a required field and must be numeric (id)'),
     body(`state_id`).isInt().notEmpty().withMessage('Department is a required field and must be numeric (id)'),
     body(`city_id`).isInt().notEmpty().withMessage('City is a required field and must be numeric (id)'),
-    body(`neighborhood`).isString().notEmpty().withMessage('Neighborhood is a required field and must be string'),
-    body(`address`).isString().notEmpty().withMessage('Address is a required field and must be string'),
+    body(`neighborhood`).optional().isString().withMessage('Neighborhood is a required field and must be string'),
+    body(`address`).optional().isString().withMessage('Address is a required field and must be string'),
     body(`created_by`).isInt().notEmpty().withMessage('Created_by is a required field and must be numeric (id)'),
     body(`updated_by`).isInt().notEmpty().withMessage('Updated_by is a required field and must be numeric (id)')
-
 ]
